52a5f034307abc00b62bc5333e4741b67b843f2c
Naufal Ridho H <naufal.hizamiar@traveloka.com>
2024-05-24 15:11:25 +0700

    test

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ class SnowArchival {
         let startIdx = 0;
 
         while (true) {
-            let tasks = await this.getTask('');
+            let tasks = await this.getTask('RITM0010023');
             // let tasks = await this.getTasks(startIdx, this.batchSize);
             if (tasks.length === 0) break;
 
